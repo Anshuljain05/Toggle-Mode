31b4a28cde72396eae2714e9506765a584b53fb4
Anshuljain05 <60061779+Anshuljain05@users.noreply.github.com>
2021-12-08 03:17:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+
+var toggle = document.getElementById('inner');
+
+var count = 0;
+
+function lightToBlack(){
+	toggle.style.cssText = 'transform: translate(80px); transition: 1s ease-in-out';
+	document.body.style.cssText = 'background-color: black';
+	document.getElementById('heading').style.cssText = 'color: white';
+};
+
+function blackToLight(){
+	toggle.style.cssText = 'transform: translate(0px); transition: 1s ease-in-out';
+	document.body.style.cssText = 'background-color: white';
+	document.getElementById('heading').style.cssText = 'color: black';
+};
+
+
+toggle.addEventListener('click', function(){
+	count++;
+	if(count%2 === 0){
+		blackToLight();
+	}
+	else{
+		lightToBlack();
+	}
+});
